96b2f0475f0b39f836c7b868f5b694e7bd705272
luism <luism@Luis>
2026-06-13 16:04:05 +0200

    TV jodeeeer

--- Pruebas/src/electrodomesticos/Television.java
+++ Pruebas/src/electrodomesticos/Television.java
@@ -1,5 +1,42 @@
 package electrodomesticos;
 
 public class Television extends Electrodomestico{
+	
+	double pulgadas = 20;
+	boolean TDT = false;
+	
+	Television(){
+		super();
+	}
+	
+	Television(double precio, double peso){
+		this.precio_base = precio;
+		this.peso = peso;
+	}
+	
+	Television(double precio, double peso, String color, char consumo, double pulgadas, boolean TDT){
+		super(precio, peso, color, consumo);
+		this.pulgadas = pulgadas;
+		this.TDT = TDT;
+	}
+	
+	public double getPulgadas() {
+		return this.pulgadas;
+	}
+	
+	public boolean getTDT() {
+		return this.TDT;
+	}
+	
+	public double precioFinal() {
+		double precio = super.precioFinal();
+		if(pulgadas > 40) {
+			precio = precio + (super.precioFinal() / 100 * 30);
+		}
+		if(TDT) {
+			precio = precio + 50;
+		}
+		return precio;
+	}
 
 }
